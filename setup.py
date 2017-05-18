2e1bb6c934bc17962e787a339d5fdfaf5a77c67c
Chiranth Siddappa <Chiranth.Siddappa@cosmicaes.com>
2017-05-17 21:54:38 -0400

    Preparing for docs

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from distutils.core import setup
+import os
+import codecs
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
 
 setup(name='scikit-dsp-comm',
       version='0.0.1',
       description='DSP and Comm package.',
+      long_description=read(fpath('README.md')),
       author=['Mark Wickert', 'Chiranth Siddappa'],
       author_email='mwickert@uccs.edu',
       url='https://github.com/mwickert/scikit-dsp-comm',

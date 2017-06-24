a7922852fb2ced6eab47e9096b6ef507c9bcadba
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2017-06-24 16:20:32 -0600

    Making changes to use tox

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ def fpath(name):
 def read(fname):
     return codecs.open(fpath(fname), encoding='utf-8').read()
 
+requirements = read(fpath('requirements.txt'))
 
 setup(name='scikit-dsp-comm',
       version='0.0.2',
@@ -22,11 +23,7 @@ setup(name='scikit-dsp-comm',
       packages=['sk_dsp_comm'],
       package_data={'sk_dsp_comm': ['ca1thru37.txt']},
       license='BSD',
-      install_requires=[
-          'numpy',
-          'matplotlib',
-          'scipy'
-          ],
+      install_requires=requirements.split(),
       test_suite='nose.collector',
-      tests_require=['nose','numpy']
+      tests_require=['nose','numpy', 'tox']
      )

996adaa087bca6af7d34288347556924279fb977
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2017-07-13 15:49:56 -0500

    Changing bit filp array for test

--- sk_dsp_comm/test/test_digitalcom.py
+++ sk_dsp_comm/test/test_digitalcom.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 
 import numpy as np
 from .. import digitalcom as dc
-import numpy.testing as npt
 
 
 class TestDigitalcom(TestCase):
@@ -27,37 +26,9 @@ class TestDigitalcom(TestCase):
         bits = 100
         transmit = np.ones(bits)
         receive = np.ones(bits)
-        rand_bits = np.random.rand(5)
+        rand_bits = [80, 75, 59, 3, 7]
         for rand_bit in rand_bits:
-            bit_loc = int(rand_bit * bits)
-            receive[bit_loc] -= 1
+            receive[rand_bit] -= 1
         bit_count, bit_errors = dc.bit_errors(transmit, receive)
         self.assertEqual(bit_count, bits)
         self.assertEqual(bit_errors, len(rand_bits))
-
-    def test_CIC(self):
-        b = dc.CIC(12, 3)
-        b_test = np.array([ 0.0005787 ,  0.00173611,  0.00347222,  0.00578704,  0.00868056,
-        0.01215278,  0.0162037 ,  0.02083333,  0.02604167,  0.0318287 ,
-        0.03819444,  0.04513889,  0.05092593,  0.05555556,  0.05902778,
-        0.06134259,  0.0625    ,  0.0625    ,  0.06134259,  0.05902778,
-        0.05555556,  0.05092593,  0.04513889,  0.03819444,  0.0318287 ,
-        0.02604167,  0.02083333,  0.0162037 ,  0.01215278,  0.00868056,
-        0.00578704,  0.00347222,  0.00173611,  0.0005787 ])
-        npt.assert_almost_equal(b_test, b)
-
-    def test_QAM_bb(self):
-        x_test, b_test, data_test = (np.array([ 1.00000000+1.j,  1.00000000+1.j,  1.00000000+1.j,  1.00000000+1.j,
-       -1.00000000+1.j, -1.00000000+1.j, -1.00000000+1.j, -1.00000000+1.j,
-       -1.00000000-1.j, -1.00000000-1.j, -1.00000000-1.j, -1.00000000-1.j,
-       -0.33333333-1.j, -0.33333333-1.j, -0.33333333-1.j, -0.33333333-1.j]), np.array([ 0.25,  0.25,  0.25,  0.25]), np.array([ 3.+3.j, -3.+3.j, -3.-3.j, -1.-3.j]))
-        x,b,data = dc.QAM_bb(4, 4)
-        npt.assert_almost_equal(b_test, b)
-
-    def test_QAM_bb_mod_error(self):
-        with self.assertRaisesRegexp(ValueError, 'Unknown mod_type') as QAM_err:
-            dc.QAM_bb(4, 4, mod_type='bpsk')
-
-    def test_QAM_bb_pulse_error(self):
-        with self.assertRaisesRegexp(ValueError, 'pulse shape must be src, rc, or rect') as QAM_err:
-            dc.QAM_bb(4, 4, pulse='sinc')

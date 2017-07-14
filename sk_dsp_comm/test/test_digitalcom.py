60a970fe955e5c48cbc4216f62b6d5afd134e4d0
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2017-07-14 16:27:49 -0500

    Adding CIC filter test

--- sk_dsp_comm/test/test_digitalcom.py
+++ sk_dsp_comm/test/test_digitalcom.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 from .. import digitalcom as dc
+from numpy import testing as npt
 
 
 class TestDigitalcom(TestCase):
@@ -32,3 +33,28 @@ class TestDigitalcom(TestCase):
         bit_count, bit_errors = dc.bit_errors(transmit, receive)
         self.assertEqual(bit_count, bits)
         self.assertEqual(bit_errors, len(rand_bits))
+
+    def test_CIC_4(self):
+        """
+        4 taps, 7 sections
+        :return:
+        """
+        b_test = np.array([  6.10351562e-05,   4.27246094e-04,   1.70898438e-03,
+         5.12695312e-03,   1.23901367e-02,   2.52075195e-02,
+         4.44335938e-02,   6.88476562e-02,   9.48486328e-02,
+         1.17065430e-01,   1.29882812e-01,   1.29882812e-01,
+         1.17065430e-01,   9.48486328e-02,   6.88476562e-02,
+         4.44335938e-02,   2.52075195e-02,   1.23901367e-02,
+         5.12695312e-03,   1.70898438e-03,   4.27246094e-04,
+         6.10351562e-05])
+        b = dc.CIC(4, 7)
+        npt.assert_almost_equal(b_test, b)
+
+    def test_CIC_1(self):
+        """
+        4 taps, 1 section
+        :return:
+        """
+        b_test = np.ones(4) / 4
+        b = dc.CIC(4, 1)
+        npt.assert_almost_equal(b_test, b)

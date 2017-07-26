9abd0f09e4727d40ffab15281efc4f6d944e7790
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2017-07-25 18:35:04 -0600

    Fixing doc string example

--- sk_dsp_comm/digitalcom.py
+++ sk_dsp_comm/digitalcom.py
@@ -926,9 +926,12 @@ def my_psd(x,NFFT=2**10,Fs=1):
     
     Examples
     --------
-    >>> x,b, data = NRZ_bits(10000,10)
-    >>> Px,f = my_psd(x,2**10,10)
-    >>> plot(f, 10*log10(Px))
+    >>> import matplotlib.pyplot as plt
+    >>> from sk_dsp_comm import digitalcom as dc
+    >>> from numpy import log10
+    >>> x,b, data = dc.NRZ_bits(10000,10)
+    >>> Px,f = dc.my_psd(x,2**10,10)
+    >>> plt.plot(f, 10*log10(Px))
 
     """
     Px,f = pylab.mlab.psd(x,NFFT,Fs)

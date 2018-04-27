d6c47d60db913d65038683f6c6134f1faa674732
Chiranth Siddappa <Chiranth.Siddappa@cosmicaes.com>
2018-04-26 23:55:42 -0600

    Missed dc xcorr; Refs mwickert/#8

--- sk_dsp_comm/digitalcom.py
+++ sk_dsp_comm/digitalcom.py
@@ -1098,7 +1098,7 @@ def xcorr(x1,x2,Nlags):
     r12 = np.fft.ifft(X1*np.conj(X2))/np.sqrt(E1*E2)
     k = np.arange(K) - int(np.floor(K/2))
     r12 = np.fft.fftshift(r12)
-    idx = mlab.find(abs(k) <= Nlags)
+    idx = np.nonzero(np.ravel(abs(k) <= Nlags))
     return r12[idx], k[idx]
 
 

127699493934371a092efd8032c8a50cdf019da7
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-04-29 21:48:00 -0600

    Adding example for conv_Pb_bound

--- sk_dsp_comm/fec_conv.py
+++ sk_dsp_comm/fec_conv.py
@@ -502,8 +502,27 @@ def conv_Pb_bound(R,dfree,Ck,SNRdB,hard_soft,M=2):
     dfree: Free distance of the code
     Ck: Weight coefficient
     SNRdB: Signal to noise ratio in dB
-    hard_soft:
+    hard_soft: 0 hard, 1 soft, 2 uncoded
     M: M-ary
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sk_dsp_comm import fec_conv as fec
+    >>> import matplotlib.pyplot as plt
+    >>> SNRdB = np.arange(2,12,.1)
+    >>> Pb = fec.conv_Pb_bound(1./2,10,[36, 0, 211, 0, 1404, 0, 11633],SNRdB,2)
+    >>> Pb_1_2 = fec.conv_Pb_bound(1./2,10,[36, 0, 211, 0, 1404, 0, 11633],SNRdB,1)
+    >>> Pb_3_4 = fec.conv_Pb_bound(3./4,4,[164, 0, 5200, 0, 151211, 0, 3988108],SNRdB,1)
+    >>> plt.semilogy(SNRdB,Pb)
+    >>> plt.semilogy(SNRdB,Pb_1_2)
+    >>> plt.semilogy(SNRdB,Pb_3_4)
+    >>> plt.axis([2,12,1e-7,1e0])
+    >>> plt.xlabel(r'$E_b/N_0$ (dB)')
+    >>> plt.ylabel(r'Symbol Error Probability')
+    >>> plt.legend(('Uncoded BPSK','R=1/2, K=7, Soft','R=3/4 (punc), K=7, Soft'),loc='best')
+    >>> plt.grid();
+    >>> plt.show()
 
     Notes
     -----

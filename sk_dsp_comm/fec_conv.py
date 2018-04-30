50e0c4ad41ed7bc2bdf7808c802a26ac432edfb0
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-04-29 23:05:07 -0600

    Added doc string for depuncture

--- sk_dsp_comm/fec_conv.py
+++ sk_dsp_comm/fec_conv.py
@@ -379,13 +379,42 @@ class fec_conv(object):
     def depuncture(self,soft_bits,puncture_pattern = ('110','101'),
                    erase_value = 3.5):
         """
-        y = depuncture(soft_bits,puncture_pattern = ('110','101'),
-                       erase_value = 4)
         Apply de-puncturing to the soft bits coming from the channel. Erasure bits
         are inserted to return the soft bit values back to a form that can be
-        Viterbi decoded.  
+        Viterbi decoded.
+
+        :param soft_bits:
+        :param puncture_pattern:
+        :param erase_value:
+        :return:
+
+        Examples
+        --------
+        This example uses the following puncture matrix:
+
+        .. math::
+
+           \\begin{align*}
+               \\mathbf{A} = \\begin{bmatrix}
+                1 & 1 & 0 \\\\
+                1 & 0 & 1
+                \\end{bmatrix}
+           \\end{align*}
+
+        The upper row operates on the outputs for the :math:`G_{1}` polynomial and the lower row operates on the outputs of
+        the  :math:`G_{2}`  polynomial.
+
+        >>> import numpy as np
+        >>> from sk_dsp_comm.fec_conv import fec_conv
+        >>> cc = fec_conv(('101','111'))
+        >>> x = np.array([0, 0, 1, 1, 1, 0, 0, 0, 0, 0])
+        >>> state = '00'
+        >>> y, state = cc.conv_encoder(x, state)
+        >>> yp = cc.puncture(y, ('110','101'))
+        >>> cc.depuncture(yp, ('110', '101'), 1)
+        array([ 0., 0., 0., 1., 1., 1., 1., 0., 0., 1., 1., 0., 1., 1., 0., 1., 1., 0.]
         """
-        # Check to see that the length of soft_bits is consistent with a rate 
+        # Check to see that the length of soft_bits is consistent with a rate
         # 1/2 code.
         L_pp = len(puncture_pattern[0])
         L_pp1 = len([g1 for g1 in puncture_pattern[0] if g1 == '1'])
@@ -393,8 +422,8 @@ class fec_conv(object):
         #L_pp0 = len([g1 for g1 in pp1 if g1 == '0'])
         N_softwords = int(np.floor(len(soft_bits)/float(2)))
         if 2*N_softwords != len(soft_bits):
-            print('Number of soft bits must be even!')
-            print('Truncating bits to be compatible.')
+            warnings.warn('Number of soft bits must be even!')
+            warnings.warn('Truncating bits to be compatible.')
             soft_bits = soft_bits[:2*N_softwords]
         # Extract the G1p and G2p encoded bits from the serial stream.
         # Assume the stream is of the form [G1p G2p G1p G2p ...   ],
@@ -407,9 +436,9 @@ class fec_conv(object):
         # puncture length period of the soft bits
         N_punct_periods = int(np.floor(N_softwords/float(L_pp1)))
         if L_pp1*N_punct_periods != N_softwords:
-            print('Number of soft bits per puncture period is %d' % L_pp1)
-            print('The number of soft bits is not a multiple')
-            print('Truncating soft bits to be compatible.')
+            warnings.warn('Number of soft bits per puncture period is %d' % L_pp1)
+            warnings.warn('The number of soft bits is not a multiple')
+            warnings.warn('Truncating soft bits to be compatible.')
             x_G1 = x_G1[:L_pp1*N_punct_periods]
             x_G2 = x_G2[:L_pp1*N_punct_periods]
         x_G1 = x_G1.reshape(N_punct_periods,L_pp1)

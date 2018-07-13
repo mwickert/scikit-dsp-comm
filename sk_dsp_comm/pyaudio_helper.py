e700e1701c8ef482574d12ef6e1a9dc208593085
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-07-13 14:16:00 -0500

    Fixing in out check function and warning text

--- sk_dsp_comm/pyaudio_helper.py
+++ sk_dsp_comm/pyaudio_helper.py
@@ -101,15 +101,15 @@ class DSP_io_stream(object):
         if not self.in_idx in devices:
             raise OSError("Input device is unavailable")
         in_check = devices[self.in_idx]
-        if not devices:
+        if not self.out_idx in devices:
             raise OSError("Output device is unavailable")
         out_check = devices[self.out_idx]
         if((in_check['inputs'] == 0) and (out_check['outputs']==0)):
             warnings.warn('Invalid input and output devices')
         elif(in_check['inputs'] == 0):
-            warnings.warn('Invalid input device')
+            warnings.warn('Selected input device has no inputs')
         elif(out_check['outputs'] == 0):
-            warnings.warn('Invalid output device')
+            warnings.warn('Selected output device has no outputs')
         return True
         
     def interaction(self,Stream):
